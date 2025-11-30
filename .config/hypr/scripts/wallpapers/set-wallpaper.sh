bf568b3b48d8a70ded5963767db3d884414bb893
akiidjk <dragonfaiver@gmail.com>
2025-11-30 23:13:20 +0100

    Set wallpaper using awww and English messages

--- .config/hypr/scripts/wallpapers/set-wallpaper.sh
+++ .config/hypr/scripts/wallpapers/set-wallpaper.sh
@@ -1,37 +1,29 @@
 #!/bin/bash
 set -eu
 
-# Папка с обоями
 WALL_DIR="$HOME/.config/wallpapers"
 
-# Проверяем, что папка существует
 if [ ! -d "$WALL_DIR" ]; then
-    echo "Папка с обоями не найдена: $WALL_DIR"
+    echo "Wallpaper folder not found: $WALL_DIR"
     exit 1
 fi
 
-# Формируем список файлов (только имена)
 FILE_LIST=$(find "$WALL_DIR" -type f \( -iname "*.png" -o -iname "*.jpg" \) -printf "%f\n")
 
-# Открываем wofi меню
 SELECTED_FILE=$(echo "$FILE_LIST" | wofi --dmenu --prompt "Select wallpaper")
 
-# Если пользователь отменил выбор
 [ -z "$SELECTED_FILE" ] && exit 1
 
-# Полный путь к выбранному файлу
 WALL="$WALL_DIR/$SELECTED_FILE"
 
 cp "$WALL" "$HOME/.current_wallpaper"
 
 echo "Setting wallpaper: $SELECTED_FILE"
 
-# Устанавливаем обои через hyprpaper
 echo "setting $WALL as wallpaper"
 awww img $WALL --transition-type wipe
 echo "Wallpaper set successfully"
 
-# Применяем pywal
 if command -v wal >/dev/null 2>&1; then
     echo "Applying pywal colors..."
     wal -i "$WALL"
