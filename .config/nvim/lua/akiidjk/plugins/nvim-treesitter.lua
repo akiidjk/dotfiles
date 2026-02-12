2e2a2cdb0232116cfe8422b59378c5d52a80b8c0
akiidjk <dragonfaiver@gmail.com>
2026-02-12 22:11:30 +0100

    Upate nvim config

--- .config/nvim/lua/akiidjk/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/akiidjk/plugins/nvim-treesitter.lua
@@ -1,14 +1,22 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = { "lua", "vim", "vimdoc", "python", "go", "typescript", "javascript" },
+      sync_install = false,
+      auto_install = true,
+
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+
+      indent = {
+        enable = true
+      }
+    })
+  end
 }
