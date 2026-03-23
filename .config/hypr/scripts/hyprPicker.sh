0522feec83902970fb7446645b2435bdaf985143
akiidjk <akiidjk@proton.me>
2026-03-23 17:10:42 +0100

    Use system hyprpicker and remove bundled copy

--- .config/hypr/scripts/hyprPicker.sh
+++ .config/hypr/scripts/hyprPicker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-~/.config/hypr/scripts/hyprpicker --format hex | head -c -1 | wl-copy
+hyprpicker --format hex | head -c -1 | wl-copy
 convert -size 100x100 xc:$(wl-paste) /tmp/color.png
 notify-send -i /tmp/color.png "Copied to your clipboard!" "$(wl-paste)"
